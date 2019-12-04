83586c4d47054831dc193af6a7004a3aaad901a7
Jackalob <nike9712@gmail.com>
2019-12-04 12:33:22 +0800

    Add_getRepos func & searched func & data

--- src/script.js
+++ src/script.js
@@ -1,15 +1,51 @@
-new Vue({
-  el:'#app',
-  data:{
-    status: true
+Vue.component("searchComponent", {
+  template: `
+    <div>ddd</div>
+  `,
+  props:{
+    data:{
+      required: true
+    }
   }
-})
+});
 
+new Vue({
+  el: "#app",
+  data: {
+    status: false,
+    username: 'jackalob',
+    data: [],
+    searched:[],
+    userExistence: null,
+  },
+  methods: {
+    addToSearched(){
+      if(this.searched.indexOf(this.username) == -1) this.searched.unshift(this.username)
+    },
+    getName(){
 
-axios.get('https://api.github.com/users/solomonxie')
-.then(res=>{
-  console.log(res.data)
-})
-.catch(err=>{
-  console.log(err)
-})
+    },
+    getRepos() {
+      axios
+        .get(`https://api.github.com/users/${this.username}/repos`)
+        .then(res => {
+          console.log(res.data);
+          this.data = res.data
+          this.userExistence = true
+          this.addToSearched()
+        })
+        .catch(err => {
+          this.data = []
+          this.userExistence = false
+        });
+      
+    }
+  },
+  beforeMount() {
+    this.getRepos()
+  },
+  watch:{
+    searched(){},
+    data(){}
+  }
+});
